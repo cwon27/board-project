18bada1902fd3398a48511f51b50299a498c1064
cwon27 <sing2727@naver.com>
2025-02-13 23:55:50 +0900

    [25.02.13 11:55] 카테고리 데이터 GET 완성 commit

---
+++ src/main/java/board/board_project/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package board.board_project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private String comm_cd; //공통코드
+    private String comm_cd_nm; //공통코드명
+}
--- src/main/java/board/board_project/service/BoardService.java
+++ src/main/java/board/board_project/service/BoardService.java
@@ -1,4 +1,11 @@
 package board.board_project.service;
 
-public interface BoardService {
+import board.board_project.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    @Autowired
+    BoardMapper boardMapper;
 }
